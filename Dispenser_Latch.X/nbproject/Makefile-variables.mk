73ef33d667e790f74b4f766a22ec962af83872e1
Abdelrahman_Elnahrawy <abd0elna7rawy@gmail.com>
2024-10-22 19:08:23 +0300

    A

---
+++ Dispenser_Latch.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,14 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Dispenser_Latch.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Dispenser_Latch.X.production.hex
+# Config_2 configuration
+CND_ARTIFACT_DIR_Config_2=dist/Config_2/production
+CND_ARTIFACT_NAME_Config_2=Dispenser_Latch.X.production.hex
+CND_ARTIFACT_PATH_Config_2=dist/Config_2/production/Dispenser_Latch.X.production.hex
