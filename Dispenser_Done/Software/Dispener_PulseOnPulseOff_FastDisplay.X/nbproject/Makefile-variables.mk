e93f23ef3b28a4cf93453dcac5e9b801a70941c2
Abdelrahman_Elnahrawy <abd0elna7rawy@gmail.com>
2024-10-22 19:13:09 +0300

    N

---
+++ Dispenser_Done/Software/Dispener_PulseOnPulseOff_FastDisplay.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Dispener_PulseOnPulseOff_FastDisplay.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Dispener_PulseOnPulseOff_FastDisplay.X.production.hex
